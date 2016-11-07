d467e6db3848a4bd49bd2290b0e87391398ef18f
Bahadir <bahadrix@gmail.com>
2016-11-07 12:49:12 +0300

    assets package

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y wget
+REMOTE_ROOT="https://raw.githubusercontent.com/bahadrix/Debian8Base/master"
+
+sudo apt-get install -y curl
 
 echo "Creating directories"
 
 mkdir -p  ~/.vim/autoload
 mkdir -p ~/.vim/bundle
 
+curl "$REMOTE_ROOT/assets/colors/"
+
+
+{ src: 'assets/colors/pathogen.vim', dest: '~/.vim/autoload/pathogen.vim' }
+     - { src: 'assets/colors/vim-colors-solarized', dest: '~/.vim/bundle/' }
+     - { src: 'assets/colors/dircolors.256dark', dest: '/etc/dircolors.256dark' }
